9ddaefcdc78be01ba8d5682e29219a005511d577
Kyujin Cho <thy2134@gmail.com>
2017-11-07 13:08:48 +0900

    Change core.php
    This commit changes php db connect initiaion setting.

--- php/include/core.php
+++ php/include/core.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO("mysql:dbname=".$_POST['db_name'].";host=127.0.0.1", "test_account", "q1w2e3r4");
+    $db = new PDO("mysql:dbname=WebProject;host=127.0.0.1", "test_account", "q1w2e3r4");
     $db->exec("SET NAMES utf8");
 ?>
