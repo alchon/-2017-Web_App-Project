69aabf78270fff04e7643cd3245751fc5aec23de
Unknown <jun97y@gmail.com>
2017-12-05 22:05:16 +0900

    yeah!

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+function setOpened() {
+    var drawer = document.getElementById("drawer");
+    var blocker = document.getElementById("blocker");
+    drawer.classList.add("opened");
+    blocker.classList.add("opened");
+}
+
+function removeOpened() {
+    if(location.hash != "#drawer"){
+        var drawer = document.getElementById("drawer");
+        var blocker = document.getElementById("blocker");
+        drawer.classList.remove("opened");
+        blocker.classList.remove("opened");
+    }
+}
+
+window.onload = () => {
+    $("drawer-toggle").onclick = setOpened;
+    $("blocker").onclick = removeOpened;
+}
