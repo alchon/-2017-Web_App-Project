f54002932a79d565c33f36553f1962598ca2bc1f
Kyujin Cho <thy2134@gmail.com>
2017-12-05 14:21:59 +0900

    Add getNearby API

--- php/api/restaruants/getNearby.php
+++ php/api/restaruants/getNearby.php
@@ -1,3 +1,16 @@
 <?php
-    echo '[]';
+    include __DIR__ . '/../../include/core.php';
+    $result = array();
+    $lat = $_GET['lat'];
+    $lng = $_GET['lng'];
+    
+    $sql = 'SELECT * FROM store WHERE SQRT(POWER(ABS(lat - '.$lat.'), 2) + POWER(ABS(lng - '.$lng.'), 2)) <= 0.0008;';
+    $rows = $db->query($sql);
+
+    foreach($rows as $row) {
+        array_push($result, $row['ID']);
+    }
 ?>
+<div>
+    <? echo json_encode($result) ?>
+</div>
