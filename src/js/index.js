83d4215d968e02fcd7f279cf91cf60fa66f10fb0
Unknown <jun97y@gmail.com>
2017-12-06 21:38:45 +0900

    basic event handle

--- src/js/index.js
+++ src/js/index.js
@@ -17,4 +17,25 @@ function removeOpened() {
 window.onload = () => {
     $("drawer-toggle").onclick = setOpened;
     $("blocker").onclick = removeOpened;
+    $("box").onmousemove = (event) => {
+        popup(event.clientX, event.clientY);
+    }
+    $("box").onmouseover = () => {
+        console.log(11);
+        $("popup").setStyle({
+            display: "block"
+        });
+    };
+    $("box").onmouseout = () => {
+          $("popup").setStyle({
+            display: "none"
+        });  
+    }
+}
+
+function popup(x, y) {
+    $("popup").setStyle({
+        top:y-60,
+        left:x-100
+    });
 }
