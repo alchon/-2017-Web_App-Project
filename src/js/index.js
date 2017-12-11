923bb64fef76abb2fa468ffb2b1ee8bbc4130eae
JoonHyukYang <jun97y@gmail.com>
2017-12-11 23:11:29 +0900

    event handling

--- src/js/index.js
+++ src/js/index.js
@@ -14,28 +14,78 @@ function removeOpened() {
     }
 }
 
+function getStore(event) {
+    new Ajax.Request("url",
+    {
+        method: "get",
+        parameters: {id : event.target.getAttribute("id")},
+        onSuccess: (ajax) => {
+            return JSON.parse(ajax.responseText);
+        },
+        // onSuccess: loadStore,
+        onFailure: ajaxFailed,
+        onException: ajaxFailed
+    });
+}
+
+function loadStore(ajax) {
+    while ($("popup").firstChild){
+        $("popup").removeChild($("popup").firstChild);
+    }
+
+    var store = JSON.parse(ajax.responseText);
+
+    var title = document.createElement("h1");
+    title.innerText = store.title;
+
+    var category = document.createElement("p");
+    category.innerText = store.category;
+    
+    $("popup").appendChild(title);
+    $("popup").appendChild(category);
+}
+
+function ajaxFailed(ajax, exception) {
+    var errorMessage = "Error making Ajax request:\n\n";
+    if (exception) {
+        errorMessage += "Exception: " + exception.message;
+    } else {
+        errorMessage += "Server status:\n" + ajax.status + " " + ajax.statusText + 
+                        "\n\nServer response text:\n" + ajax.responseText;
+    }
+    console.log(errorMessage);
+}
+
+function popupRefresh(x, y) {
+    $("popup").setStyle({
+        top:y,
+        left:x
+    });
+}
+
 window.onload = () => {
     $("drawer-toggle").onclick = setOpened;
     $("blocker").onclick = removeOpened;
-    $("box").onmousemove = (event) => {
-        popup(event.clientX, event.clientY);
+    
+    var boxes = $$(".box");
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].onmousemove = (event) => {
+            var x = event.pageX - 100;
+            var y = event.pageY - 60;
+            popupRefresh(x,y);
+            // popup(event.pageX, event.pageY);
+        }
+        boxes[i].onmouseover = (event) => {
+            var store = getStore(event);
+            
+            $("popup").setStyle({
+                display: "block"
+            });
+        };
+        boxes[i].onmouseout = () => {
+              $("popup").setStyle({
+                display: "none"
+            });  
+        }
     }
-    $("box").onmouseover = () => {
-        console.log(11);
-        $("popup").setStyle({
-            display: "block"
-        });
-    };
-    $("box").onmouseout = () => {
-          $("popup").setStyle({
-            display: "none"
-        });  
-    }
-}
-
-function popup(x, y) {
-    $("popup").setStyle({
-        top:y-60,
-        left:x-100
-    });
 }
