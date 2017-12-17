626fac333c8588af5a6c44c23a237b70c04f5ab2
Kyujin Cho <thy2134@gmail.com>
2017-12-17 16:48:14 +0900

    Update API

--- php/api/restaruants/getAll.php
+++ php/api/restaruants/getAll.php
@@ -1,10 +1,25 @@
 <?php
     include __DIR__ . '/../../include/core.php';
     $result = array();
-    $rows = $db->query('SELECT ID FROM store');
+    $rows = $db->query('SELECT * FROM store');
 
+    header('Content-Type: application/json');
+    
     foreach($rows as $row) {
-        array_push($result, $row['ID']);
+        $pushRow = array(
+            'ID' => $row['ID'], 
+            'name' => $row['name'], 
+            'address_1' => $row['address_1'], 
+            'address_2' => $row['address_2'], 
+            'tel' => $row['tel'], 
+            'branch' => $row['branch'], 
+            'sub_branch' => $row['sub_branch'], 
+            'lat' => $row['lat'], 
+            'lng' => $row['lng'], 
+            'x' => $row['x'], 
+            'y' => $row['y']
+        );
+        array_push($result, $pushRow);
     }
 ?>
 <? echo json_encode($result) ?>
