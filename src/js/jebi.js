4e90ea6a555447faece09e314944817a08c623ac
alchon <gusqja8753@gmail.com>
2017-12-17 16:35:31 +0900

    제비뽑기

--- src/js/jebi.js
+++ src/js/jebi.js
@@ -6,7 +6,8 @@ window.onload = () => {
 
 function jebiClick() {
     $("img").addClassName("hidden");
-    getStores_JSON();
+    ShowStores();
+    // getStores_JSON();
 }
 
 function moreClick() {
@@ -17,6 +18,10 @@ function outClick() {
     window.close();
 }
 
+// function ShowStores() {
+//     var name = 
+// }
+
 // {
 //  "stores" : [
 //              {"title":x},
@@ -25,35 +30,35 @@ function outClick() {
 //  ]
 // }
 
-function getStores_JSON() {
-    new Ajax.Request("url", {
-        method: "get",
-        parameters: {},
-        onSuccess: showStores_JSON,
-        onFailure: ajaxFailure,
-        onException: ajaxFailure
-    });
-}
+// function getStores_JSON() {
+//     new Ajax.Request("url", {
+//         method: "get",
+//         parameters: {},
+//         onSuccess: showStores_JSON,
+//         onFailure: ajaxFailure,
+//         onException: ajaxFailure
+//     });
+// }
 
-function showStores_JSON(event) {
-    var data = JSON.parse(ajax.responseText);
-    var index = Math.floor(Math.random()*(data.title.length));
-    var title = data.title[index];
-    var p = document.createElement("p");
-    var p2 = document.createElement("p");
-    p.innerHTML = title;
-    p2.innerHTML = "어떠세요??"
-    document.querySelector("h1").appendChild(p);
-    document.querySelector("p").appendChild(p2);
-}
+// function showStores_JSON(event) {
+//     var data = JSON.parse(ajax.responseText);
+//     var index = Math.floor(Math.random()*(data.title.length));
+//     var title = data.title[index];
+//     var p = document.createElement("p");
+//     var p2 = document.createElement("p");
+//     p.innerHTML = title;
+//     p2.innerHTML = "어떠세요??"
+//     document.querySelector("h1").appendChild(p);
+//     document.querySelector("p").appendChild(p2);
+// }
 
-function ajaxFailed(ajax, exception) {
-    var errorMessage = "Error making Ajax request:\n\n";
-    if (exception) {
-        errorMessage += "Exception: " + exception.message;
-    } else {
-        errorMessage += "Server status:\n" + ajax.status + " " + ajax.statusText + 
-                        "\n\nServer response text:\n" + ajax.responseText;
-    }
-    console.log(errorMessage);
-}
+// function ajaxFailed(ajax, exception) {
+//     var errorMessage = "Error making Ajax request:\n\n";
+//     if (exception) {
+//         errorMessage += "Exception: " + exception.message;
+//     } else {
+//         errorMessage += "Server status:\n" + ajax.status + " " + ajax.statusText + 
+//                         "\n\nServer response text:\n" + ajax.responseText;
+//     }
+//     console.log(errorMessage);
+// }
