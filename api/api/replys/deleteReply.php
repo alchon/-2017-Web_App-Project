b76361c63ead0a2776478ad64047d89beef5e8e7
Kyujin Cho <thy2134@gmail.com>
2017-12-19 09:17:47 +0900

    Fixing reply feature

--- api/api/replys/deleteReply.php
+++ api/api/replys/deleteReply.php
@@ -6,11 +6,12 @@
     
     $req = array();
     preg_match('/password=.+&?/', file_get_contents("php://input"), $req);
+    echo file_get_contents("php://input");
     $password = str_replace('&', '', str_replace('password=', '', $req[0]));
     $password = hash('sha256', $password);
 
     echo $password;
-    
+
     $query = $db->query("SELECT password FROM reply WHERE id=".$args['id'])->fetchAll()[0];
     if($query['password'] != $password) {
         echo json_encode(array(
